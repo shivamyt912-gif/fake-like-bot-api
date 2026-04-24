e09b1501520627959054f812813325ff04e4d07e
shivamyt912-gif <shivamyt912@gmail.com>
2026-04-24 20:04:11 +0545

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def like_api():
         print(f"Error fetching real data: {e}")
     
     # Generate fake likes (1-220)
-    like_given = random.randint(SHIVAM, 0)
+    like_given = random.randint(0.1, 0)
     after_like = before_like + like_given
     
     # Build response
